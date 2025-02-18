5959c4f52f9acd7a458e5cc9e5989152b49a41e2
Churned <123ernkhai@gmail.com>
2025-02-18 17:55:21 +0800

    minor fixes

--- script.js
+++ script.js
@@ -46,18 +46,19 @@ document.addEventListener('DOMContentLoaded', function() {
         // Handle different commands
         switch (cmd) {
             case 'start':
-                return '<div class="help-text-container"><pre class="help-text">\n>SYSTEM INFO\n>ABOUT\n>PROJECT\n>HISTORY\n>CONTACT\n>CLEAR</pre></div>';
+                return '<div class="help-text-container"><pre class="help-text">FILE DIRECTORY\n>HANDBUCH\n>SYSTEM_INFO\n>ABOUT\n>PROJECT\n>HISTORY\n>CONTACT\n>CLEAR</pre></div>';
+            case 'handbuch' :
+                return 'DAS_ARBEITNEHMER_HANDBUCH\n\nThis terminal represents a recovered fragment of the Old World\'s technological era.\nThe data stored within may be incomplete, but it provides a glimpse into a civilization we were once in.\nThis handbook contains information on how to navigate this terminal that you may read up on during your stay in this station.\nWe appreciate your dedication to the Emancipated People\'s Union, your service is vital to the protection of our nation and the safety of humanity, the sacrifices of each citizen contribute to the continued survival of our species among the stars. We must all do our part.\nAnd remember, comrade: To live is to work, for the good of humanity.';
             case 'system info':
                 return 'OS: VEILCHEN_OS_v1.0.4\nHOST: arbeitsstation-system\nKERNEL: 2.4.32-KR\nARCHITECTURE: x86\nPROCESSOR: 2600 RL Z3 Prozessor\nCORE: 1\nMEMORY: 64 words 22 bits\nNETWORK: Verbunden (ETH0)\nLAST_RESTART: 28 Jan 1949\nSYSTEM_TIME: ' + n + ':'+ m +':' + s + ' UTC';
-                
-            case 'ABOUT':
+            case 'about':
+                return '<div class="help-text-container"><pre class="help-text">SYSTEM INFORMATION - PERSONNEL FILE NO. 486\n-------------------------------------------\nNAME:           Ernki\nRANK:           Intern\nDESIGNATION:    Veilchen Architect\nSTATION:        *Classified*\nSTATUS:         ACTIVE - Deployment Ready</pre></div>' + '\n\nTECHNICAL PROFILE\n-------------------------------------------\n';
+            case 'project':
+                return '<div class="help-text-container"></div>';
+            case 'history':
+                return '<div class="help-text-container"></div>';
+            case 'contact':
                 return '';
-            case 'PROJECT':
-                return '<div class="help-text-container"></div>';
-            case 'HISTORY':
-                return '<div class="help-text-container"></div>';
-            case 'CONTACT':
-                return 'You can reach me via email at "info@gmail.com".';
             case 'clear':
                 clearTerminal();
                 return ''; // Return empty string after clearing terminal
@@ -72,8 +73,9 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function clearTerminal() {
-        output.innerHTML = ''; // Clear the output content
+        output.innerHTML = `<div class="output">--Geben Sie '<strong>start</strong>' ein, um zu beginnen-- <br>--Type '<strong>start</strong>' to begin--</div>`; // Clear the output content
         // Scroll to the top after clearing
+        userInput.value = '';
         terminalBody.scrollTop = 0;
     }
 
@@ -101,10 +103,11 @@ document.addEventListener('DOMContentLoaded', function() {
                     element.innerHTML += currentChar;
                     i++;
                 }
-                setTimeout(type, 50); // Adjust typing speed here
+                setTimeout(type, 25); // Adjust typing speed here
             }
         }
 
         type();
     }
+    
 });
